0b16d0eabb1586d27e03f310c497cf7cf9682cba
Glenn Kroeze <0890210@hr.nl>
2018-05-28 17:34:40 +0200

    Move all audio to audio service

--- audio.py
+++ audio.py
@@ -1,18 +1,18 @@
 from pyo import *
 from threading import Thread
 
-
 class Audio:
 
     def __init__(self):
-        pa_list_devices()
-        self.thread = Thread(target=)
+        self.SNDS_PATH = 'camera/python-sound-lib/audioFiles/'
+        # pa_list_devices()
         self.server = Server(audio='portaudio', sr=44100, nchnls=2, duplex=0)
         # self.server.setOutputDevice(6)
         self.server.boot()
         while(not self.server.getIsBooted()):
             pass
-            
+
+        self.player = SfPlayer(self.SNDS_PATH + "afstand.wav").out()
         self.lfo = LFO(freq=0)
         self.pan = Pan(self.lfo, spread=0)
 
@@ -26,9 +26,16 @@ class Audio:
         self.STATE = 'STOPPED'
 
     def start(self):
-        self.server.start()
-        while(not self.server.getIsStarted()):
-            pass
+        if not self.server.getIsStarted():
+            self.server.start()
+            while not self.server.getIsStarted():
+                pass
 
         self.pan.out()
         self.STATE = 'RUNNING'
+
+    def playSample(self, name, pan):
+        self.player.setPath(self.SNDS_PATH + name + '.wav')
+        self.player.out()
+
+
